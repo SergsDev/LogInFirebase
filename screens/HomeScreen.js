a89d24adc1d1dc2af3bf8b35e852e2d0c8285cf8
Sergs <sergsdev20@gmail.com>
2022-11-15 15:45:05 -0500

    aaa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground,Image } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
 import { useNavigation } from '@react-navigation/native'
+import { TextInput } from 'react-native-gesture-handler'
 
 const HomeScreen = () => {
 
@@ -20,16 +21,27 @@ const HomeScreen = () => {
   const changeDeliveryScreen = () => {
     navigation.navigate('deliveryScreen')
   }
+
+  const image = {uri: 'https://i.imgur.com/Px03Guk.png'};
+
   return (
     <View style={styles.container}>
-      <Text>Esta es la homescreen de: {auth.currentUser?.email}</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.imagebg}>
+        
+      
+      <Text style={styles.textGreet}>Tus medicamentos, Faciles!</Text>
+      <TextInput placeholder='Buscar medicinas' style={styles.search}></TextInput>
+
+      <Image style={styles.flyer} source={{uri: 'https://i.imgur.com/rsBfFNT.png'}}></Image>
+      </ImageBackground>
+      {/* <Text>Esta es la homescreen de: {auth.currentUser?.email}</Text>
       <TouchableOpacity style={styles.button} onPress={handleSignOut}>
-        <Text style={styles.buttonText}>Log Out</Text>
+      <Text style={styles.buttonText}>Log Out</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={changeDeliveryScreen}>
         <Text style={styles.buttonText}>Cambiar a DeliveryScreen</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   )
 }
@@ -39,7 +51,8 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     alignItems: 'center'
   },
 
@@ -57,4 +70,35 @@ buttonText:{
     fontWeight: '700',
     fontSize: 18,
 },
+    imagebg:{
+      flex:1,
+      width:'100%',
+    },
+
+    textGreet:{
+      paddingTop: 100,
+      width: '80%',
+      fontFamily: 'Roboto',
+      color: '#012030',
+      fontWeight: 'bold',
+      fontSize: 26,
+      margin: 15,
+    },
+    search:{
+      padding: 15,
+      backgroundColor: '#f2f2f2',
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: '##012030',
+      margin: 15,
+    },
+
+    flyer: {
+      
+      width: '100%',
+      margin: 15,
+      height: 150,
+      borderWidth: 1,
+      borderRadius: 10,
+    }
 })
